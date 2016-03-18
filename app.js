7249b3e3bd7f957d1632f3b29f76f780fa4a4fb0
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-03-18 09:21:37 -0700

    app.js updated with code for problem 2

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-// problem 1 create a sum() function-practice
+// problem 1 create a sum() function
 function sum(sumNumber1, sumNumber2) {
   var sumAnswer = sumNumber1 + sumNumber2;
   console.log('The sum of ' + sumNumber1 + ' and ' + sumNumber2 + ' is ' + sumAnswer + '.');
+  return;
+}
+
+// problem 2 create a multiply() function
+function multiply(multiplyNumber1, multiplyNumber2) {
+  var multiplyAnswer = multiplyNumber1 * multiplyNumber2;
+  console.log('The product of ' + multiplyNumber1 + ' and ' + multiplyNumber2 + ' is ' + multiplyAnswer + '.');
   return;
 }
