45612c77ecead8f2dd7a72b233cd2cee8a73f300
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-03-18 09:56:15 -0700

    added code to app.js for problem 3

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 // problem 1 create a sum() function
 function sum(sumNumber1, sumNumber2) {
-  var sumAnswer = sumNumber1 + sumNumber2;
-  console.log('The sum of ' + sumNumber1 + ' and ' + sumNumber2 + ' is ' + sumAnswer + '.');
-  return;
+  sumAnswer = sumNumber1 + sumNumber2;
+  //console.log('The sum of ' + sumNumber1 + ' and ' + sumNumber2 + ' is ' + sumAnswer + '.');
+  return sumAnswer;
 }
 
 // problem 2 create a multiply() function
 function multiply(multiplyNumber1, multiplyNumber2) {
-  var multiplyAnswer = multiplyNumber1 * multiplyNumber2;
-  console.log('The product of ' + multiplyNumber1 + ' and ' + multiplyNumber2 + ' is ' + multiplyAnswer + '.');
-  return;
+  multiplyAnswer = multiplyNumber1 * multiplyNumber2;
+  //console.log('The product of ' + multiplyNumber1 + ' and ' + multiplyNumber2 + ' is ' + multiplyAnswer + '.');
+  return multiplyAnswer;
+}
+
+// problem 3 create function sumAndMultiply()
+function sumAndMultiply(x, y, z) {
+  sumAndMultiplyAnswer = [sum(sum(x,y),z), multiply(multiply(x,y),z)];
+  console.log('The sum of ' + x + ', ' + y + ' and ' + z + ' is ' + sumAndMultiplyAnswer[0] + '.');
+  console.log('The product of ' + x + ', ' + y + ' and ' + z + ' is ' + sumAndMultiplyAnswer[1] + '.');
+  return sumAndMultiplyAnswer;
 }
