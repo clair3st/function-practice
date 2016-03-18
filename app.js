018e6301bad0adb3fb32f315e834a4b9a1021d2f
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-03-18 09:00:32 -0700

    app.js updated with code for problem one

---
+++ app.js
@@ -0,0 +1,6 @@
+// problem 1 create a sum() function-practice
+function sum(sumNumber1, sumNumber2) {
+  var sumAnswer = sumNumber1 + sumNumber2;
+  console.log('The sum of ' + sumNumber1 + ' and ' + sumNumber2 + ' is ' + sumAnswer + '.');
+  return;
+}
